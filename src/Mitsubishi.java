be8e4638d4842b6cbf4ff483f3574dc7fdcf6528
Tom Fegan <tomfegan@Toms-MacBook-Air.local>
2022-06-10 09:15:45 +0100

    First commit

---
+++ src/Mitsubishi.java
@@ -0,0 +1,17 @@
+public class Mitsubishi extends Car {
+    public Mitsubishi(int cylinders, String name) {
+        super(cylinders, name);
+    }
+    @Override
+    public String startEngine() {
+        return "Mitsubishi -> startEngine()";
+    }
+    @Override
+    public String accelerate() {
+        return getClass().getSimpleName() + " -> accelerate()";
+    }
+    @Override
+    public String brake() {
+        return "Mitsubishi -> brake()";
+    }
+}
